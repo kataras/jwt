73e44207406cb70d72e0811f042fd1ba65536f2a
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2023-08-17 11:58:19 +0300

    update to go1.21

--- util.go
+++ util.go
@@ -4,5 +4,5 @@ import "unsafe"
 
 // BytesToString converts a slice of bytes to string without memory allocation.
 func BytesToString(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
+	return unsafe.String(unsafe.SliceData(b), len(b))
 }
